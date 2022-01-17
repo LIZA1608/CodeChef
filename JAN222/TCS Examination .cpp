f8b1c9bfa11dd08e39af1b81c58c347b69affe8f
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-01-17 20:10:25 +0530

    Create TCS Examination .cpp

---
+++ JAN222/TCS Examination .cpp
@@ -0,0 +1,31 @@
+//Problem Code: EXAMTIME
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >>t;
+    while(t--){
+        int a,b,c,d,e,f;
+        cin>>a>>b>>c>>d>>e>>f;
+        if(a+b+c>d+e+f){
+            cout<<"DRAGON\n";
+        }
+        else if(a+b+c<d+e+f){
+            cout<<"SLOTH\n";
+        }
+        else{
+            if(a>d) cout<<"DRAGON\n";
+            else if(d>a) cout<<"SLOTH\n";
+            else
+            {
+                if (b>e) cout<<"DRAGON\n";
+                else if(e>b) cout <<"SLOTH\n";
+                else cout<<"TIE\n";
+        }
+        }
+    }
+	
+	return 0;
+}
